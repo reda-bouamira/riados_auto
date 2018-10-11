d890f9c214f1081392c47d23653ae3335c57d6cf
Reda Bouamira <reda-bouamira@outlook.fr>
2018-10-11 12:30:17 -0500

    created the model Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_172755) do
+ActiveRecord::Schema.define(version: 2018_10_11_172929) do
 
   create_table "makes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "models", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
