e95aebec8f0f3c570ea79c7b9dbd101fa9d78bfd
Reda Bouamira <reda-bouamira@outlook.fr>
2018-10-12 10:56:06 -0500

    Before Database drop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'vehicles/index'
+  get 'vehicles', to:'vehicles#index'
+  
+  root to: 'vehicles#index'
   get 'vehicles/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/about', to: 'pages#about', as: 'about'
