dd2593c9dfd6613d466da889e6f5844a3f0b31b3
Reda Bouamira <reda-bouamira@outlook.fr>
2018-10-11 23:38:16 -0500

    Created teh vehicles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'vehicles/index'
+  get 'vehicles/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages/about', to: 'pages#about'
+  get 'pages/about', to: 'pages#about', as: 'about'
   get 'about', to: 'pages#about'
 end
